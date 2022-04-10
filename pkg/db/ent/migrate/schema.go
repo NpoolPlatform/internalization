20ead9b539f87a56bd028d992176b23cac8775f5
Zhao KK <kikakkz@hotmail.com>
2022-04-10 05:23:02 +0000

    update schema

--- pkg/db/ent/migrate/schema.go
+++ pkg/db/ent/migrate/schema.go
@@ -70,7 +70,7 @@ var (
 		{Name: "app_id", Type: field.TypeUUID},
 		{Name: "message_id", Type: field.TypeString},
 		{Name: "lang_id", Type: field.TypeUUID},
-		{Name: "message", Type: field.TypeString, Size: 1024},
+		{Name: "message", Type: field.TypeString, Size: 16016},
 		{Name: "batch_get", Type: field.TypeBool},
 		{Name: "create_at", Type: field.TypeUint32},
 		{Name: "update_at", Type: field.TypeUint32},
